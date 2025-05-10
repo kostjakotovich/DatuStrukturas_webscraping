294ec3ca69102e8043e377ecfbd53a25d77278ef
Konstantīns Kotovičs <kostja.kotovich@gmail.com>
2025-05-10 21:53:57 +0300

    1)pievienots menu prieks ertibas 2) Pievienoti 2 dictionary prieks website iebuvetiem filtriem un lietotaja personigiem filtriem 3) Prieks lietotaja personigiem filtriem ir pievienots tads parametrs ka experience_level (piredzes limenis piem  'Junior')

--- main.py
+++ main.py
@@ -1,25 +1,70 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 adress = "https://www.visidarbi.lv"
 
-html_page = requests.get(adress)
+all_vacances = []
+website_filters = {}
+personal_filters = {}
 
-if html_page.status_code == 200:
-    page_content = BeautifulSoup(html_page.text, "html.parser")
-    vacancies = page_content.find('div', class_ = ['item', 'item big-item'])
-    # print(vacancies)
-    if vacancies:
-        company = vacancies.find('li', class_ = 'company')
-        if company:
-            company_name = company.text.strip()
-            speciality = vacancies.find('div', class_='title').find('a').text.strip()
-            print(f'Company Name: {company_name}\nSpeciality: {speciality}\n')
-            
-        else:
-            print("Comapny not found")
-    else:
-        print("Vacancy not found")
+def set_website_filters():
+    print("")
+    speciality = input("Ievadiet vēlamo specializāciju (ja nevēlaties norādīt, nospiediet 'ENTER' bez teksta): \n")
+    print("")
+    website_filters["speciality"] = speciality
+    return website_filters
 
+def set_personal_filters():
+    experience_level = input("Ievadiet Jūsu pieredze līmeni (ja nevēlaties norādīt, nospiediet 'ENTER' bez teksta): \n")
+    print("")
+    personal_filters["experience_level"] = experience_level
+    return personal_filters
 
+def find_vacancies():
+    html_page = requests.get(adress)
+
+    if html_page.status_code == 200:
+        page_content = BeautifulSoup(html_page.text, "html.parser")
+        vacancies = page_content.find_all('div', class_ = ['item', 'item big-item'])
+
+        for vacancy in vacancies:
+            # print(vacancies)
+            if vacancy:
+                company = vacancy.find('li', class_ = 'company')
+                if company:
+                    vacancy_title = vacancy.find('div', class_='title').find('a').text.strip()
+                    if personal_filters['experience_level'].lower() in vacancy_title.lower():
+                        company_name = company.text.strip()
+                        vacancy_link = vacancy.find('a', class_='long-title')['href']
+                        if vacancy_link.startswith('/'):
+                            vacancy_link = adress + vacancy_link
+                        print(f"Company Name: {company_name}\nVacancy Title: {vacancy_title}")
+                        print(f"Link to vacancy: {vacancy_link}\n")
+                    
+                else:
+                    print("Comapny not found")
+            else:
+                print("Vacancy not found")
+
+if __name__ == "__main__":
+    set_website_filters()
+    set_personal_filters()
+    while True:
+        find_vacancies()
+        menu_number = input("Menu (write the corresponding number of the command you want to execute):\n1) Change job parameters\n2) Refresh all vacancies\n3) Exit \n")
+        match menu_number:
+            case "1":
+                set_website_filters()
+                find_vacancies()
+            case "2":
+                wait_time = 5
+                print(f"Waiting {wait_time} seconds for vacancies refreshing...\n")
+                time.sleep(wait_time)
+                find_vacancies()
+            case "3":
+                break
+            case _: 
+                print("Unknown command, please type 1 or 2")
+ 
         
